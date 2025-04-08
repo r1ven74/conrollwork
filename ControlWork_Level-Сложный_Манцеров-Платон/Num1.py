7d9dca5322355b0f08ac1539e73a7212564d1731
r1ven74 <mancerovplaton@gmail.com>
2025-04-08 09:49:10 +0700

    Num1.py

--- ControlWork_Level-Сложный_Манцеров-Платон/Num1.py
+++ ControlWork_Level-Сложный_Манцеров-Платон/Num1.py
@@ -1,21 +1,32 @@
-#№1
+# Определяем базовый класс Animal
 class Animal:
+    # Метод make_sound, который будет переопределен в подклассах
     def make_sound(self):
         print("Животное издает звук")
 
+# Определяем класс Kot, который наследует от Animal
 class Kot(Animal):
+    # Переопределяем метод make_sound для класса Kot
     def make_sound(self):
         print("Кошка мяукает")
 
+# Определяем класс Dog, который также наследует от Animal
 class Dog(Animal):
+    # Переопределяем метод make_sound для класса Dog
     def make_sound(self):
         print("Собака лает")
 
+# Создаем экземпляр базового класса Animal
 animal = Animal()
-animal.make_sound()
+# Вызываем метод make_sound для объекта animal
+animal.make_sound()  # Вывод: Животное издает звук
 
+# Создаем экземпляр класса Dog
 dog = Dog()
-dog.make_sound()
+# Вызываем метод make_sound для объекта dog
+dog.make_sound()  # Вывод: Собака лает
 
+# Создаем экземпляр класса Kot
 kot = Kot()
-kot.make_sound()
+# Вызываем метод make_sound для объекта kot
+kot.make_sound()  # Вывод: Кошка мяукает
